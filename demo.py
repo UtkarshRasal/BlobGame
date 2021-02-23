0300bd008eca5ee57ced9ac7c995e26035d66bec
Utkarsh Rasal <utkarshrasal@gmail.com>
2021-02-23 18:42:12 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+example  = ['left', 'right','up','down']
+
+# for i in range(len(example)):
+#     print(i, example[i])
+	
+# 	OR ( above can also be rewritten as)	
+
+for i, j in enumerate(example):
+    print(i,j)
